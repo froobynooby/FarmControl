5cdc795e2cb4d994fc0f0db8a7d4ccf0af4d6652
froobynooby <froobynooby@froobworld.com>
2022-02-12 15:42:33 +0930

    Use a more descriptive pie chart for modes in use

--- src/main/java/com/froobworld/farmcontrol/metrics/charts/ModesInUseChart.java
+++ src/main/java/com/froobworld/farmcontrol/metrics/charts/ModesInUseChart.java
@@ -2,35 +2,40 @@ package com.froobworld.farmcontrol.metrics.charts;
 
 import com.froobworld.farmcontrol.FarmControl;
 import com.froobworld.farmcontrol.controller.ActionProfile;
-import org.bstats.charts.SimpleBarChart;
+import org.bstats.charts.SimplePie;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class ModesInUseChart extends SimpleBarChart {
+public class ModesInUseChart extends SimplePie {
 
     public ModesInUseChart(FarmControl farmControl) {
         super("modes_used", () -> {
-            Map<String, Integer> modeCountMap = new HashMap<>();
+            boolean usesProactive = false;
+            boolean usesReactive = false;
             for (World world : Bukkit.getWorlds()) {
                 for (String profileName : farmControl.getFcConfig().worldSettings.of(world).profiles.proactive.get()) {
                     ActionProfile actionProfile = farmControl.getProfileManager().getActionProfile(profileName);
                     if (actionProfile != null) {
-                        modeCountMap.put("proactive", 1);
+                        usesProactive = true;
                         break;
                     }
                 }
                 for (String profileName : farmControl.getFcConfig().worldSettings.of(world).profiles.reactive.get()) {
                     ActionProfile actionProfile = farmControl.getProfileManager().getActionProfile(profileName);
                     if (actionProfile != null) {
-                        modeCountMap.put("reactive", 1);
+                        usesReactive = true;
                         break;
                     }
                 }
             }
-            return modeCountMap;
+            if (usesProactive && usesReactive) {
+                return "Both proactive and reactive";
+            } else if (usesProactive) {
+                return "Proactive";
+            } else if (usesReactive) {
+                return "Reactive";
+            }
+            return "None";
         });
     }
 
